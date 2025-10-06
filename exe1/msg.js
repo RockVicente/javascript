593012ec82e62632cfd53b853ccd8beccf8be747
rock vicente <rockvicente03@gmail.com>
2025-10-06 19:40:09 +0200

    Initial commit

---
+++ exe1/msg.js
@@ -0,0 +1,4 @@
+function mensagemParabens(){
+    document.getElementById("msg").innerHTML="Parabens"
+}
+ 
---
+++ exe2/index.js
@@ -0,0 +1,5 @@
+var ale = Math.random
+function numAleatorio(){ 
+    let num = Math.floor(Math.random() * 51)
+    document.getElementById("numAleatorio").innerHTML="o numero aleatorio e " + num
+}
